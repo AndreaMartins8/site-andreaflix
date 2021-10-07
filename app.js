32d20fca6a3b29a2c618467fdaf53a4c38e8391d
AndreaMartins8 <87030430+AndreaMartins8@users.noreply.github.com>
2021-10-06 21:47:08 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-function CarregarFilmes(){
-    //array
-    let filmes = ["imagens/brilhoeterno.jpg" , "imagens/interestelar.jpg" , "imagens/vingadores.jpg" , "imagens/blackmirror.jpg" , "imagens/babydriver.jpg" , "imagens/galaxy.jpg"]
+ //array --> variável com subdivisões (índices)
+    let filmes = ["imagens/brilhoeterno.jpg", "imagens/interestelar.jpg", "imagens/vingadores.jpg", "imagens/babydriver.jpg" , "imagens/galaxy.jpg" ]
+//refaturação -->melhorar um código existente
+function CarregarFilmes(){ 
+    let totalFilmes = filmes.length
+    let divListaFilmes = document.querySelector(".lista-filmes")
 
+    divListaFilmes.innerHTML = ""
     //laço de repetição
-    for(let i = 0 ; i < 6 ; i = i +1){
-        document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes [i] + ">"
+    for (let i = 0; i < totalFilmes; i++) {
+        divListaFilmes.innerHTML += "<img src=" + filmes[i] + ">"
     }
-  
+}
+
+function AdicionarFilme() {
+    let nomeFilme = document.querySelector("#nome-do-filme").value
+    let caminhoCompleto = "imagens/" + nomeFilme
+
+    filmes.push(caminhoCompleto)
+    CarregarFilmes()
+
+    console.table(filmes)
 }
